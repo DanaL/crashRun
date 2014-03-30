3997370b2b3edd0afbeac2730917d6a2b2689ee0
Dana <ywg.dana@gmail.com>
2014-03-30 07:32:18 -0500

    New map generator that makes maps that are rooms connected by corridors.

---
+++ src/RLDungeonGenerator.py
@@ -0,0 +1,238 @@
+# Copyright 2014 by Dana Larose
+
+# This file is part of crashRun.
+
+# crashRun is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# crashRun is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with crashRun.  If not, see <http://www.gnu.org/licenses/>.
+
+from math import sqrt
+from random import random
+from random import randrange
+from random import choice
+
+from . import Terrain
+from .Terrain import TerrainFactory
+from .Terrain import DOOR
+from .Terrain import FLOOR
+from .Terrain import WALL
+
+class Room:
+    def __init__(self, r, c, h, w):
+        self.row = r
+        self.col = c
+        self.height = h
+        self.width = w
+
+class RLDungeonGenerator:
+    def __init__(self, w, h):
+        self.MAX = 15 # Cutoff for when we want to stop dividing sections
+        self.width = w
+        self.height = h
+        self.leaves = []
+        self.map = []
+        self.rooms = []
+        self.tf = TerrainFactory()
+        self.floor = self.tf.get_terrain_tile(FLOOR)
+        self.wall = self.tf.get_terrain_tile(WALL)
+
+        self.generate_initial_map()
+
+    def generate_initial_map(self):
+        for h in range(self.height):
+            row = []
+            for w in range(self.width):
+                row.append(self.wall)
+
+            self.map.append(row)
+
+    def random_split(self, min_row, min_col, max_row, max_col):
+        # We want to keep splitting until the sections get down to the threshold
+        seg_height = max_row - min_row
+        seg_width = max_col - min_col
+
+        if seg_height < self.MAX and seg_width < self.MAX:
+            self.leaves.append((min_row, min_col, max_row, max_col))
+        elif seg_height < self.MAX and seg_width >= self.MAX:
+            self.split_on_vertical(min_row, min_col, max_row, max_col)
+        elif seg_height >= self.MAX and seg_width < self.MAX:
+            self.split_on_horizontal(min_row, min_col, max_row, max_col)
+        else:
+                if random() < 0.5:
+                    self.split_on_horizontal(min_row, min_col, max_row, max_col)
+                else:
+                    self.split_on_vertical(min_row, min_col, max_row, max_col)
+     
+    def split_on_horizontal(self, min_row, min_col, max_row, max_col):
+        split = (min_row + max_row) // 2 + choice((-2, -1, 0, 1, 2))
+        self.random_split(min_row, min_col, split, max_col)
+        self.random_split(split + 1, min_col, max_row, max_col)
+
+    def split_on_vertical(self, min_row, min_col, max_row, max_col):        
+        split = (min_col + max_col) // 2 + choice((-2, -1, 0, 1, 2))
+        self.random_split(min_row, min_col, max_row, split)
+        self.random_split(min_row, split + 1, max_row, max_col)
+
+    def carve_rooms(self):
+        for leaf in self.leaves:
+            # We don't want to fill in every possible room or the 
+            # dungeon looks too uniform
+            if random() > 0.80: continue
+            section_width = leaf[3] - leaf[1]
+            section_height = leaf[2] - leaf[0]
+
+            # The actual room's height and width will be 60-100% of the 
+            # available section. 
+            room_width = round(randrange(60, 100) / 100 * section_width)
+            room_height = round(randrange(60, 100) / 100 * section_height)
+
+            # If the room doesn't occupy the entire section we are carving it from,
+            # 'jiggle' it a bit in the square
+            if section_height > room_height:
+                room_start_row = leaf[0] + randrange(section_height - room_height)
+            else:
+                room_start_row = leaf[0]
+
+            if section_width > room_width:
+                room_start_col = leaf[1] + randrange(section_width - room_width)
+            else:
+                room_start_col = leaf[1]
+    
+            self.rooms.append(Room(room_start_row, room_start_col, room_height, room_width))
+            for r in range(room_start_row, room_start_row + room_height):
+                for c in range(room_start_col, room_start_col + room_width):
+                    self.map[r][c] = self.floor
+
+    def are_rooms_adjacent(self, room1, room2):
+        adj_rows = []
+        adj_cols = []
+        for r in range(room1.row, room1.row + room1.height):
+            if r >= room2.row and r < room2.row + room2.height:
+                adj_rows.append(r)
+
+        for c in range(room1.col, room1.col + room1.width):
+            if c >= room2.col and c < room2.col + room2.width:
+                adj_cols.append(c)
+
+        return (adj_rows, adj_cols)
+
+    def distance_between_rooms(self, room1, room2):
+        centre1 = (room1.row + room1.height // 2, room1.col + room1.width // 2)
+        centre2 = (room2.row + room2.height // 2, room2.col + room2.width // 2)
+
+        return sqrt((centre1[0] - centre2[0]) ** 2 + (centre1[1] - centre2[1]) ** 2)
+
+    def carve_corridor_between_rooms(self, room1, room2):
+        if room2[2] == 'rows':
+            row = choice(room2[1])
+            # Figure out which room is to the left of the other
+            if room1.col + room1.width < room2[0].col:
+                start_col = room1.col + room1.width
+                end_col = room2[0].col
+            else:
+                start_col = room2[0].col + room2[0].width
+                end_col = room1.col                
+            for c in range(start_col, end_col):
+                self.map[row][c] = self.floor
+
+            if end_col - start_col >= 4:
+                self.map[row][start_col] = self.tf.get_terrain_tile(DOOR)
+                self.map[row][end_col - 1] = self.tf.get_terrain_tile(DOOR)
+            elif start_col == end_col - 1:
+                self.map[row][start_col] = self.tf.get_terrain_tile(DOOR)
+        else:
+            col = choice(room2[1])
+            # Figure out which room is above the other
+            if room1.row + room1.height < room2[0].row:
+                start_row = room1.row + room1.height
+                end_row = room2[0].row
+            else:
+                start_row = room2[0].row + room2[0].height
+                end_row = room1.row
+
+            for r in range(start_row, end_row):
+                self.map[r][col] = self.floor
+
+            if end_row - start_row >= 4:
+                self.map[start_row][col] = self.tf.get_terrain_tile(DOOR)
+                self.map[end_row - 1][col] = self.tf.get_terrain_tile(DOOR)
+            elif start_row == end_row - 1:
+                self.map[start_row][col] = self.tf.get_terrain_tile(DOOR)
+
+    # Find two nearby rooms that are in difference groups, draw
+    # a corridor between them and merge the groups
+    def find_closest_unconnect_groups(self, groups, room_dict):
+        shortest_distance = 99999
+        start = None
+        start_group = None
+        nearest = None
+
+        for group in groups:
+            for room in group:
+                key = (room.row, room.col)
+                for other in room_dict[key]:
+                    if not other[0] in group and other[3] < shortest_distance:
+                        shortest_distance = other[3]
+                        start = room
+                        nearest = other
+                        start_group = group
+
+        self.carve_corridor_between_rooms(start, nearest)
+
+        # Merge the groups
+        other_group = None
+        for group in groups:
+            if nearest[0] in group:
+                other_group = group
+                break
+
+        start_group += other_group
+        groups.remove(other_group)
+        
+    def connect_rooms(self):
+        # Build a dictionary containing an entry for each room. Each bucket will
+        # hold a list of the adjacent rooms, weather they are adjacent along rows or 
+        # columns and the distance between them.
+        #
+        # Also build the initial groups (which start of as a list of individual rooms)
+        groups = []
+        room_dict = {}
+        for room in self.rooms:
+            key = (room.row, room.col)
+            room_dict[key] = []
+            for other in self.rooms:
+                other_key = (other.row, other.col)
+                if key == other_key: continue
+                adj = self.are_rooms_adjacent(room, other)
+                if len(adj[0]) > 0:
+                    room_dict[key].append((other, adj[0], 'rows', self.distance_between_rooms(room, other)))
+                elif len(adj[1]) > 0:
+                    room_dict[key].append((other, adj[1], 'cols', self.distance_between_rooms(room, other)))
+        
+            groups.append([room])
+    
+        while len(groups) > 1:
+            self.find_closest_unconnect_groups(groups, room_dict)
+
+    def generate_map(self):
+        self.random_split(1, 1, self.height - 1, self.width - 1)
+        self.carve_rooms()
+        self.connect_rooms()
+
+    def print_map(self):
+        for r in range(self.height):
+            row = ''
+            for c in range(self.width):
+                ch = self.map[r][c].get_ch()
+                row += ' ' if ch == '.' else ch
+            print(row)
+
